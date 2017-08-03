ca2e870ee713fe12d92e182fd813abbf22cdd524
mengshixing <shixingmeng@gmail.com>
2017-08-03 18:07:25 +0800

    type

--- www/orm.py
+++ www/orm.py
@@ -18,13 +18,16 @@ async def create_pool(loop,**kw):
     #http://aiomysql.readthedocs.io/en/latest/pool.html
     __pool=await aiomysql.create_pool(
     
-        host=kw.get('host','localhost'),
-        port=kw.get('port','3306'),
+        host=kw.get('host','127.0.0.1'),
+        #端口号不能输入字符串.self.host_info = "socket %s:%d" % (self._host, self._port)
+        #TypeError: %d format: a number is required, not str
+        port=kw.get('port',3306),
         user=kw.get('user'),
         password=kw.get('password'),
         db=kw.get('db'),
         
-        charset=kw.get('charset','utf-8'),
+        #此处utf8不能写成utf-8否则'Connection' object has no attribute '_writer'        
+        charset=kw.get('charset','utf8'),
         # MySQL默认操作模式就是autocommit自动提交模式。这就表示除非显式地开始一个事务，
         # 否则每个查询都被当做一个单独的事务自动执行
         # MySQL默认的存储引擎是MyISAM，MyISAM存储引擎不支持事务处理，所以改变autocommit没有什么作用。
@@ -79,9 +82,102 @@ async def execute(sql,args):
             await cur.close()
         except BaseException as e:
             raise
+        #执行完SQL语句,释放与数据库的连接。    
+        finally:
+            conn.close()
+        #print(affect)
         return affect
     
+# 定义字段基类
+# 在我们编写一个新的Python类的时候，总是在最开始位置写一个初始化方法__init__，
+# 以便初始化对象，然后会写一个__str__方法，方面我们调试程序。
+class Field(object):
+    def __init__(self,name,column_type,primary_key,default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+        
+    def __str__(self):
+        return '<%s,%s:%s>' % (self.__class__.__name__,self.column_type,self.name)
+        
+class StringField(Field):
+    def __init__(self,name=None,primary_key=False,default=None,ddl='varchar(100)'):
+        super().__init__(name,ddl,primary_key,default)
+        
+class BooleanField(Field):
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
 
+class IntegerField(Field):
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)   
+    
+# id = StringField(primary_key=True, default='2222', ddl='varchar(50)')
+# if __name__=='__main__':
+    # print(id);  输出<StringField,varchar(50):None>
+    
+    
+#通过metaclass：ModelMetaclass：将具体的子类如User的映射信息读出
+# >>> type(type)  <class 'type'> ，`type`是一个元类`metaclass`
+#metaclass 元类,控制类的创建行为,类可以看成是元类创建的实例,动态修改类
+class ModelMetaclass(type):
+    def __new__(cls,name,bases,attrs):
+        if name='Model':#排除Model类
+            return type.__new__(cls,name,bases,attrs)
+        
+        tableName=attrs.get('__table__',None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+        
+        # 获取所有的Field和主键名:
+		mappings=dict()
+		fields=[]
+		primaryKey=None
+		for k,v in attrs.items():#查找Field属性放入字典
+            if isinstance(v,Field):
+				mappings[k]=v
+                logging.info('found mapping: %s ==> %s' % (k, v))
+				if v.primary_key:#如果是主键
+					if primaryKey:
+						#主键重复了
+						raise RuntimeError('Duplicate primary key for field: %s' % k)
+					primaryKey = k
+				else:					 
+					fields.append(k)
+		if not primaryKey:
+			#遍历完之后发现没有主键
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():#从类属性中删除Field属性
+            attrs.pop(k)    
+		#把不含主键的段组成字符串数组	
+		escaped_fields=list(map(lambda f:"'%s'" % f,fields))
+        attrs['__mappings__']=mappings #保存属性和列的映射关系
+        attrs['__table__']=tableName #表名简化为类名
+		attrs['__primary_key__'] = primaryKey # 主键属性名
+        attrs['__fields__'] = fields # 除主键外的属性名集合
+		
+		# 构造默认的SELECT, INSERT, UPDATE和DELETE语句:
+        attrs['__select__'] = 'select `%s‘, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+		
+		
+        return type.__new__(cls,name,bases,attrs)
+		
+		
+		
+		
+            
+        
 class Model():
     pass
     
--- www/test.py
+++ www/test.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 
-#import asyncio,logging
-#import aiomysql
+import asyncio,logging
+import aiomysql
 
 import orm
 import time, uuid
@@ -13,10 +13,25 @@ print (uuid.uuid4().hex)
 print (int(time.time() * 1000), uuid.uuid4().hex)
 #'%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
+# async def test_save():
+    # user = User(id="123", name="Caisw")
+    # await user.save()
+
+
+# async def test_find():
+    # user = await User.find("123")
+    # print(user)
+
+
 print ('%015d' % int(time.time() * 10000000000))
 
 loop=asyncio.get_event_loop()
 
-loop.run_until_complete()
+loop.run_until_complete(orm.create_pool(loop=loop,user='root',password='root',db='blog'))
 
+loop.run_until_complete(orm.execute('select * from users',()))
+
+#loop.run_until_complete(asyncio.wait([orm.create_pool(loop=loop,user='root',password='root',db='blog'),orm.execute('select * from users',())]))
+#__pool.close()
+#loop.run_until_complete(__pool.wait_closed())
 loop.close()
